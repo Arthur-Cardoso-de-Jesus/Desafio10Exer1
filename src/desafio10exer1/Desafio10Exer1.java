cbf76c520861642fdd89ebd4a67ec1b4cbfae93e
Arthur Cardoso de Jesus <arthurcardosoj@gmail.com>
2021-05-20 15:29:10 -0300

    Uma atividade feita no dia 13/05/2021 Loop simples que mostra numeros impares entre 100 e 200 usando o coman do for

---
+++ src/desafio10exer1/Desafio10Exer1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desafio10exer1;
+
+/**
+ *
+ * @author baung
+ */
+public class Desafio10Exer1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      for (int cont = 101; cont <200; cont+=2){
+          System.out.println(cont);
+      }
+    }
+    
+}
